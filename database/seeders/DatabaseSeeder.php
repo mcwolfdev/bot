8bb80e66495a3f195a4a916be586d69b181de7e6
mcwolfdev <mcwolf.mcwolf@gmail.com>
2022-06-16 01:24:54 +0300

    first

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,7 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -20,10 +20,16 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
-        DB::table('users')->insert([
-            'name' => Str::random(5),
-            'email' => Str::random(5).'@gmail.com',
-            'password' => Hash::make('password'),
-        ]);
+        $user_db = (User::where('name', 'admin')->first());
+        if (empty($user_db)){
+            User::table('users')->create([
+                'name' => Str::random(5),
+                'email' => Str::random(5).'@gmail.com',
+                'password' => Hash::make('password'),
+            ]);
+        } else {
+            return;
+        }
+
     }
 }
--- database/seeders/textanswerSeeder.php
+++ database/seeders/textanswerSeeder.php
@@ -2,9 +2,7 @@
 
 namespace Database\Seeders;
 
-use App\Models\Debug;
 use App\Models\TextAnswer;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class textanswerSeeder extends Seeder
